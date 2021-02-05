037ac4cbcc822c95cfe076f038da4e454bc64f9f
LKarre <liselotte.karre@hm.com>
2021-02-05 13:47:54 +0100

    New version for subtraction

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
+
+
         System.out.println("Simple calculator");
-    }
+
+    double A = 22;
+    double B = 15;
+        System.out.println(A-B);
+
+        }
 }
