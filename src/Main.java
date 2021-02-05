ceb652060133d5713daaba1c0cd1f0ad7b636b1c
thomasHM21 <thomas.forsberg@hm.com>
2021-02-05 13:47:27 +0100

    Added addNumbers method to Main

--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Simple calculator");
     }
+
+    //Metod för att addera och returnera två st integers
+    public static int addNumbers(int a, int b){
+        return a + b;
+    }
 }
