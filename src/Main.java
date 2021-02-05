2e9c0966b1215d8cc934ef7e850af5ab53898f2d
albertomikai <alberto.mikai@hm.com>
2021-02-05 13:47:47 +0100

    new version from Alberto

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,13 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Simple calculator");
+
+
+        double A = 10;
+        double B = 4;
+
+        System.out.println(A * B);
+
+
     }
 }
